a53138a0afe7a452628904d28f22eaef6555134b
Sud0nym4315 <49741866+Sud0nym4315@users.noreply.github.com>
2019-09-13 19:58:38 -0400

    Add files via upload

---
+++ rot47.c
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+int main() {
+	cout << "\033[2J"; //clear screen
+
+	char message[];
+	char *msg = message;	//pointer to char array "message"
+
+	cout << "Message: ";
+	cin >> *msg;		//take input for char array "message"
+
+	while(*msg) {		
+		*msg += 47;	//increment letter by 47
+		msg += 1;		//move to the next letter
+	}
+
+	cout << message;		//print encrypted message
