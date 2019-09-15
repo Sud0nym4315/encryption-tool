e735069d07955564afef9e0674633478cb2313c2
Sud0nym4315 <49741866+Sud0nym4315@users.noreply.github.com>
2019-09-15 18:57:44 -0400

    Update rot47.c

--- rot47.c
+++ rot47.c
@@ -6,13 +6,13 @@ int main() {
 	cout << "\033[2J"; //clear screen
 
 	char message[];
-	char *msg = message;	//pointer to char array "message"
+	char *msg = message;		//pointer to char array "message"
 
 	cout << "Message: ";
-	cin >> *msg;		//take input for char array "message"
+	cin >> *msg;			//take input for char array "message"
 
 	while(*msg) {		
-		*msg += 47;	//increment letter by 47
+		*msg += 47;		//increment letter by 47
 		msg += 1;		//move to the next letter
 	}
 
